65434d3fdb0951a8caae6c4cf75f1a1ea985bf4c
Kengo TODA <skypencil@gmail.com>
2022-04-28 06:57:04 +0800

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,9 @@
+plugins {
+    java
+}
+
+// https://docs.gradle.org/7.4.2/userguide/working_with_files.html#sec:reproducible_archives
+tasks.withType<AbstractArchiveTask>().configureEach {
+    isPreserveFileTimestamps = false
+    isReproducibleFileOrder = true
+}
